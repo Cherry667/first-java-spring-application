beb0d1c6e1c5ba2f8fe09427e97369da97a9974b
Kuba <j.czerwinski03@wp.pl>
2025-01-10 21:59:18 +0100

    Initial commit

---
+++ first-java-spring-application/src/main/java/pl/edu/vistula/first_java_spring_application/FirstJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_java_spring_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaSpringApplication.class, args);
+	}
+
+}
